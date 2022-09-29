6501683d019992c45e435097404f798bf98f694d
Owen Brooks <owen.h.brooks@gmail.com>
2022-09-29 14:37:13 +1000

    Ensure pose_estimate_to_tf runs continuously
    Transforms need to be broadcast regularly for them to be accepted.

--- velocity_planner/velocity_planner/pose_estimate_to_tf.py
+++ velocity_planner/velocity_planner/pose_estimate_to_tf.py
@@ -1,16 +1,13 @@
-from tarfile import LENGTH_NAME
+"""
+Takes input from the "2D Pose Estimate" button in rviz2 and publishes this as a tf2 transform to the "base_link" frame,
+as well as to the /current_pose topic. This is helpful for debugging the planner.
+"""
 import rclpy
 import logging
 from rclpy.node import Node
-
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from geometry_msgs.msg import TransformStamped
-from mcav_interfaces.msg import WaypointArray, Waypoint, DetectedObjectArray
-import numpy as np
-import math
-import tf2_ros
 from tf2_ros import TransformBroadcaster
-from velocity_planner.vector_ops import q_normalise, qv_mult
 
 class PoseEstimateToTf(Node):
 
@@ -19,15 +16,28 @@ class PoseEstimateToTf(Node):
 
         # Subscriber
         timer_period = 0.01  # seconds
-        self.spinner = self.create_timer(timer_period, self.spin)
+        self.spinner = self.create_timer(timer_period, self.broadcast_tf)
         self.initial_pose_sub = self.create_subscription(PoseWithCovarianceStamped,
             'initialpose', self.initial_pose_callback, 10)
         self.initial_pose_sub  # prevent unused variable warning
 
         # Publisher
         self.current_pose_pub = self.create_publisher(PoseWithCovarianceStamped, 'current_pose', 10)
+
+        self.tf_broadcaster = TransformBroadcaster(self)
+        self.current_pose = None
+        self.current_tf = None
         
         self.get_logger().set_level(logging.DEBUG)
+
+    def broadcast_tf(self):
+        if self.current_tf is not None:
+            self.current_tf.header.stamp = self.get_clock().now().to_msg()
+            self.current_pose.header.stamp = self.get_clock().now().to_msg()
+            # Send the transformation
+            self.tf_broadcaster.sendTransform(self.current_tf)
+            # Publish the pose msg of type PoseWithCovarianceStamped
+            self.current_pose_pub.publish(self.current_pose)
 
 
     def initial_pose_callback(self, pose_msg: PoseWithCovarianceStamped):
@@ -35,21 +45,20 @@ class PoseEstimateToTf(Node):
 
         # Read message content and assign it to
         # corresponding tf variables
-        t.header = pose_msg.header
         # Publish a transform from map to base_link
-        t.header.frame_id = 'map'
+        t.header.frame_id = pose_msg.header.frame_id
         t.child_frame_id = 'base_link'
 
         # Set transform translation to msg pose position
-        t.transform.translation = pose_msg.pose.pose.position
+        t.transform.translation.x = pose_msg.pose.pose.position.x
+        t.transform.translation.y = pose_msg.pose.pose.position.y
+        t.transform.translation.z = pose_msg.pose.pose.position.z
 
         # Set transform rotation to pose msg orientation
         t.transform.rotation = pose_msg.pose.pose.orientation
 
-        # Send the transformation
-        self.tf_broadcaster.sendTransform(t)
-        # Publish the pose msg of type PoseWithCovarianceStamped
-        self.current_pose_pub.publish(pose_msg)
+        self.current_tf = t
+        self.current_pose = pose_msg
 
 
 def main(args=None):
