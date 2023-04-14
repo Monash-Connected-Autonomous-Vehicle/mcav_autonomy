1a726a63dec8cd6ba26457872ae2ab35bb5e3786
Owen Brooks <owen.h.brooks@gmail.com>
2023-04-14 13:21:02 +1000

    Simplify tmux start script now that we don't need ros1bridge

--- docker/tmux-start.sh
+++ docker/tmux-start.sh
@@ -10,13 +10,4 @@ sudo sh -c "echo \"127.0.0.1	`hostname`.localdomain	`hostname`\" >> /etc/hosts" 
 session="mcav"
 tmux new-session -d -s $session
 
-tmux split-window -h
-tmux send-keys './autonomy_launch/scripts/2.sh'
-tmux split-window -v
-tmux send-keys './autonomy_launch/scripts/3.sh'
-tmux split-window -v -t 1
-tmux send-keys './autonomy_launch/scripts/4.sh'
-tmux select-pane -t 1
-tmux send-keys './autonomy_launch/scripts/1.sh'
-
 tmux attach-session -d -t $session
