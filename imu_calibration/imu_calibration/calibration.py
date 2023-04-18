958c6f171edd9eb012ceb92c2a5ca72eaa7e19d4
liam wise <wiseliam7@gmail.com>
2023-04-18 17:20:46 +1000

    Add launch file for imu calibration package

--- imu_calibration/imu_calibration/calibration.py
+++ imu_calibration/imu_calibration/calibration.py
@@ -7,20 +7,23 @@ class CalibrationPublisher(Node):
     FRAME_ID = 657
     
     def __init__(self, x=0, y=0, z=1):
-        super.__init__('calibration_publisher')
+        super().__init__('calibration_publisher')
         self.publisher_ = self.create_publisher(Frame, '/to_can_bus', 1)
-        self.data = [x, y, z, 1]
-        self.send_message()
+        data = [x, y, z, 1, 0, 0, 0, 0]
+        self.data = [2 if i == -1 else i for i in data] 
+        time_period = 0.5
+        self.create_timer(time_period, self.send_message)
     
     def send_message(self):
         msg = Frame()
         msg.id = self.FRAME_ID
         msg.data = self.data
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: ID: {} Data: {}'.format(self.FRAME_ID, ''.join(msg.data)))
+        self.get_logger().info('Publishing: ID: {} Data: {}'.format(self.FRAME_ID, msg.data))
 
 
 def main():
+    rclpy.init()
     calibrator = CalibrationPublisher(1, 0, -1)
     rclpy.spin(calibrator)
     calibrator.destroy_node()
