c2febfb9e2f5e8c2a694abfe73675e3d11a5bb08
Owen Brooks <owen.h.brooks@gmail.com>
2023-04-21 17:27:01 +1000

    Add git committer script

---
+++ login.sh
@@ -0,0 +1,6 @@
+export GIT_COMMITTER_NAME="Owen Brooks"
+export GIT_COMMITTER_EMAIL="owen.h.brooks@gmail.com"
+export GIT_AUTHOR_NAME="$GIT_COMMITTER_NAME"
+export GIT_AUTHOR_EMAIL="$GIT_COMMITTER_EMAIL"
+
+echo "Committer name and email set to: '$GIT_COMMITTER_NAME' <$GIT_COMMITTER_EMAIL>"
