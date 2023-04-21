488eef6203498be927916e56a50410924e720308
Srihari Sridhar <ssrihari2002@gmail.com>
2023-04-22 01:36:32 +1000

    Added necessary dependencies. Code accounts for tensor size and uses CompressedImage msg instead of Image. Issue with visualization. Unable to set the msg frame_id to camera.

--- project_tracker/project_tracker/semantic_segmentation.py
+++ project_tracker/project_tracker/semantic_segmentation.py
@@ -12,34 +12,46 @@ import numpy as np
 from rclpy.node import Node
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 
 class SemanticSegmentation(Node):
     def __init__(self):
         super(SemanticSegmentation, self).__init__('semantic_segmentation')
-        self._subscriber = self.create_subscription(Image, '/image_downsampled', self._callback, 10)
-        self._lane_publisher = self.create_publisher(Image, '/lane_image', 10)
-        self._dz_publisher = self.create_publisher(Image, '/drivable_zone_image', 10)
+        self._subscriber = self.create_subscription(CompressedImage, '/gmsl_camera/port_0/cam_0/image_raw/compressed', self._callback, 10)
+        self._lane_publisher = self.create_publisher(CompressedImage, '/lane_image', 10)
+        self._dz_publisher = self.create_publisher(CompressedImage, '/drivable_zone_image', 10)   
         
         self.get_logger().set_level(logging.DEBUG)
-        self.model = torch.hub.load('hustvl/yolop', 'yolop', pretrained=True, source='local')
+        # self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s') 
+        # self.model = torch.hub.load('hustvl/yolop', 'yolop', pretrained=True, source='local')
+        self.model = torch.hub.load('hustvl/yolop', 'yolop')
+        self.get_logger().debug("At the innit function")
 
-    def _callback(self, msg: Image):
+
+    def _callback(self, msg: CompressedImage):
+        msg.header.frame_id = "camera"
         bridge = CvBridge()
-        cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-        _, dz_out, lane_out = self.model(cv_image)
+        cv_image = bridge.compressed_imgmsg_to_cv2(msg, desired_encoding="passthrough")
+        cv_image_12ch = np.repeat(cv_image[:, :, np.newaxis, :], 12, axis=2)
+        cv_image_tensor = torch.from_numpy(cv_image_12ch[..., :3]).permute(0, 3, 1, 2).float()/255.0
+        _, dz_out, lane_out = self.model(cv_image_tensor)
+        self.get_logger().debug("model loaded")
 
         # Reversing colour
         dz_image_array = np.array(dz_out.render()[0])[:,:,::-1]
-        dz_image_message = bridge.cv2_to_imgmsg(dz_image_array, encoding="passthrough")
+        
+        dz_image_message = bridge.compressed_imgmsg_to_cv2(dz_image_array, desired_encoding="passthrough")
         dz_image_message.header.frame_id = msg.header.frame_id
 
         lane_image_array = np.array(lane_out.render()[0])[:,:,::-1]
-        lane_image_message = bridge.cv2_to_imgmsg(lane_image_array, encoding="passthrough")
+        lane_image_message = bridge.compressed_imgmsg_to_cv2(lane_image_array, desired_encoding="passthrough")
         lane_image_message.header.frame_id = msg.header.frame_id
-
+        self.get_logger().debug("Reversing colours")
+        
         # Publishing segmented image
-        self._publisher.publish(dz_image_message)
-        self._publisher.publish(lane_image_message)
+        self._lane_publisher.publish(dz_image_message)
+        self._dz_publisher.publish(lane_image_message)
+        self.get_logger().debug("Publishing images")
 
 def main(args = None):
     rclpy.init(args=args)
