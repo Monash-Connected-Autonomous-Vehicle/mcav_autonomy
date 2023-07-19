Created test publisher node, encountered bug in determining quaternion from rotation matrix

localisation/localisation/pose_initialisation.py
@@ -13,6 +13,7 @@ from sensor_msgs.msg import PointCloud2
 from sensor_msgs_py import point_cloud2 as pc2
 import open3d as o3d # Open3d for ICP implementation
 import numpy as np
+import math
 
 class PoseInitialisation(Node):
     def __init__(self):
@@ -30,7 +31,7 @@ class PoseInitialisation(Node):
         self.lidar_points_sub = self.create_subscription(PointCloud2,
             '/lidar_points', self.set_lidar_points, 10)
             
-        # Publisher # TODO: determine out topic name to publish to
+        # Publisher for transform from map frame to lidar frame
         self.transform_pub = self.create_publisher(TransformStamped, '/initial_pose_transform', 10)
         timer_period = 0.1 # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
@@ -118,9 +119,9 @@ class PoseInitialisation(Node):
         s = (x**2+y**2+z**2+w**2) **-2 # Should be 1 for unit quaternions
         # TODO: Check that this was transcribed correctly
         # Alternatively: o3d.geometry.get_rotation_matrix_from_quaternion exists
-        rot_matrix = np.asarry([[1 - 2*s*(y**2 + z**2), 2*s*(x*y - z*w), 2*s*(y*z + y*w),
+        rot_matrix = np.asarry([[1 - 2*s*(y**2 + z**2), 2*s*(x*y - z*w), 2*s*(y*z + y*w)],
                                      [2*s*(x*y + z*w), 1-2*s*(x**2+z**2), 2*s*(y*z - x*w)],
-                                     [2*s*(x*z - y*w), 2*s*(y*z + x*w), 1 - 2*s*(x**2 + y**2)])
+                                     [2*s*(x*z - y*w), 2*s*(y*z + x*w), 1 - 2*s*(x**2 + y**2)]])
         return rot_matrix
         
     def rotation_matrix_to_quarternion(rot_matrix):
@@ -129,7 +130,7 @@ class PoseInitialisation(Node):
            # Using the identity of trace + 1 = 4w^2 and the unit quaternion definition
            """
         trace = rot_matrix[0, 0] + rot_matrix[1, 1] + rot_matrix[2, 2]
-        r = trace**0.5
+        r = math.sqrt(trace)
         s = 1/(2*r)
         w = 0.5*r 
         x = (rot_matrix[2, 1] - rot_matrix[1, 2])*s

localisation/localisation/rough_pose_guess.py
@@ -4,14 +4,16 @@ From a reference map and lidar pointcloud, use global registration to create an 
 
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import PoseStamped, TransformStamped, Transform, Quaternion, Vector3
+from geometry_msgs.msg import TransformStamped, Transform, Quaternion, Vector3
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs_py import point_cloud2 as pc2
-import open3d as o3d # Open3d for ICP implementation
+import open3d as o3d # Open3d for ICP and global registration implementation
 import numpy as np
+import math
 
 class RoughPoseGuess(Node):
     def __init__(self):
+        super().__init__('rough_pose_guess')
         # Subscriber to /pointcloud_map
         self.pointcloud_map_sub = self.create_subscription(PointCloud2,
             '/pointcloud_map', self.set_map, 10)
@@ -20,8 +22,8 @@ class RoughPoseGuess(Node):
         self.lidar_points_sub = self.create_subscription(PointCloud2,
             '/lidar_points', self.set_lidar_points, 10)
             
-        # Publisher for rough pose guess
-        self.pose_pub = self.create_publisher(PoseStamped, '/rough_pose_guess', 10)
+        # Publisher for rough pose guess # Or a transform?
+        self.transform_pub = self.create_publisher(TransformStamped, '/rough_pose_guess', 10)
         timer_period = 1 # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.start_time = self.get_clock().now().nanoseconds
@@ -49,14 +51,18 @@ class RoughPoseGuess(Node):
     def timer_callback(self):
         """Run global registration on the map and lidar pointclouds to find a rough alignment transformation"""
         if self.map is not None and self.lidar_points is not None:
-            # self.get_logger().info('Received inputs, finding initial pose')
+            # self.get_logger().info('Found inputs')
             
             # Convert ROS2 messages into open3D data types
             # Converting ROS2 PointCloud2 to Open3D PointCloud
-            source = o3d.geometry.PointCloud(
-                o3d.utility.Vector3dVector(pc2.read_points(self.map)))
+            nparray = pc2.read_points_numpy(self.map)
+            vector = o3d.utility.Vector3dVector(nparray)
+            pcl = o3d.geometry.PointCloud(vector)
+            source = pcl
+            # source = o3d.geometry.PointCloud(
+            #    o3d.utility.Vector3dVector(pc2.read_points(self.map)))
             target = o3d.geometry.PointCloud(
-                o3d.utility.Vector3dVector(pc2.read_points(self.lidar_points)))
+                o3d.utility.Vector3dVector(pc2.read_points_numpy(self.lidar_points)))
             
             source_down, source_fpfh = preprocess_point_cloud(source, self.voxel_size)
             target_down, target_fpfh = preprocess_point_cloud(target,self.voxel_size)
@@ -64,9 +70,12 @@ class RoughPoseGuess(Node):
                                                         source_fpfh, target_fpfh,
                                                         self.voxel_size)
             trans_matrix = result_ransac.transformation
-            rotation = rotation_matrix_to_quarternion(trans_matrix[0:3, 0:3)
-            translation = Vector3(trans_matrix[0,3],trans_matrix[1,3],trans_matrix[2,3])
-            self.rough_pose_guess = Transform(translation, rotation)
+            self.get_logger().info(f"Rotation: {trans_matrix[0:3, 0:3]}")
+            self.get_logger().info(f"Trace: {sum(np.diag(trans_matrix[0:3,0:3]))}")
+            rotation = rotation_matrix_to_quarternion(trans_matrix[0:3, 0:3])
+
+            translation = Vector3(x=trans_matrix[0,3],y=trans_matrix[1,3],z=trans_matrix[2,3])
+            self.rough_pose_guess = Transform(translation=translation, rotation=rotation)
             # Publish message
             msg = TransformStamped()
             msg.header.frame_id = 'map_transform_lidar' # This is the rough transform that places the map onto the lidar pointcloud
@@ -74,47 +83,47 @@ class RoughPoseGuess(Node):
             msg.transform = self.rough_pose_guess
             self.transform_pub.publish(msg)
             
-    def preprocess_point_cloud(pcd, voxel_size):
-        pcd_down = pcd.voxel_down_sample(voxel_size)
+def preprocess_point_cloud(pcd, voxel_size):
+    pcd_down = pcd.voxel_down_sample(voxel_size)
 
-        radius_normal = voxel_size * 2
-        pcd_down.estimate_normals(
-            o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+    radius_normal = voxel_size * 2
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
 
-        radius_feature = voxel_size * 5
-        pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
-            pcd_down,
-            o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
-        return pcd_down, pcd_fpfh
-                
-    def execute_global_registration(source_down, target_down, source_fpfh,
-                                target_fpfh, voxel_size):
-        distance_threshold = voxel_size * 1.5
-        result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
-            source_down, target_down, source_fpfh, target_fpfh, True,
-            distance_threshold,
-            o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
-            3, [
-                o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(
-                    0.9),
-                o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
-                    distance_threshold)
-            ], o3d.pipelines.registration.RANSACConvergenceCriteria(100000, 0.999))
-        return result
-        
-    def rotation_matrix_to_quarternion(rot_matrix):
-        """Input: a 3x3 rotation matrix as a numpy array
-           Output: a ROS2 Quaternion representing the same rotation
-           # Using the identity of trace + 1 = 4w^2 and the unit quaternion definition
-           """
-        trace = rot_matrix[0, 0] + rot_matrix[1, 1] + rot_matrix[2, 2]
-        r = trace**0.5
-        s = 1/(2*r)
-        w = 0.5*r 
-        x = (rot_matrix[2, 1] - rot_matrix[1, 2])*s
-        y = (rot_matrix[0, 2] - rot_matrix[2, 0])*s
-        z = (rot_matrix[1, 0] - rot_matrix[0, 1])*s
-        return Quaternion(x=x,y=y,z=z,w=w)
+    radius_feature = voxel_size * 5
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
+            
+def execute_global_registration(source_down, target_down, source_fpfh,
+                            target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 1.5
+    result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+        source_down, target_down, source_fpfh, target_fpfh, True,
+        distance_threshold,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
+        3, [
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(
+                0.9),
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
+                distance_threshold)
+        ], o3d.pipelines.registration.RANSACConvergenceCriteria(100000, 0.999))
+    return result
+    
+def rotation_matrix_to_quarternion(rot_matrix):
+    """Input: a 3x3 rotation matrix as a numpy array
+       Output: a ROS2 Quaternion representing the same rotation
+       # Using the identity of trace + 1 = 4w^2 and the unit quaternion definition
+       """
+    trace = rot_matrix[0, 0] + rot_matrix[1, 1] + rot_matrix[2, 2]
+    r = math.sqrt(trace)
+    s = 1/(2*r)
+    w = 0.5*r 
+    x = (rot_matrix[2, 1] - rot_matrix[1, 2])*s
+    y = (rot_matrix[0, 2] - rot_matrix[2, 0])*s
+    z = (rot_matrix[1, 0] - rot_matrix[0, 1])*s
+    return Quaternion(x=x,y=y,z=z,w=w)
    
 def main(args=None):
     rclpy.init(args=args)