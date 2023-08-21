194a2f2635eb8d931dc9e4208402dfec0f8747c2
Vedesh Appadoo <vedesh1408@gmail.com>
2023-08-21 17:05:35 +1000

    added loggers for each sensors

--- sensors_launch/sensors_launch/neobotix_driver.py
+++ sensors_launch/sensors_launch/neobotix_driver.py
@@ -4,6 +4,7 @@ from sensor_msgs.msg import Range
 from can_msgs.msg import Frame
 from rclpy.node import Node
 import time
+
 class NeobotixDriver(Node):
     def __init__(self):
         super().__init__('neobotix_driver')
@@ -11,11 +12,16 @@ class NeobotixDriver(Node):
         self.subscription = self.create_subscription(
             Frame, '/from_can_bus', self.callback, 10)
         self.publisher = self.create_publisher(Range, '/ultrasonic_data', 1)
+        self.get_logger().info('NeobotixDriver node initialized.')
 
     def callback(self, frame):
-        if frame.id in [0x402]:
+        if frame.id in [0x402,0x403]:
             data = frame.data
-            indices = [2]  # Indices of ultrasonic data in the frame
+            indices = [2,3,4,5]  # Indices of ultrasonic data in the frame
+            sensor_names = [
+                "Front left", "Front center", "Front Right", "Right middle",
+                "Right back", "Back center", "Left back", "Left Middle"
+            ]
             if len(data) == 8:
                 for i, index in enumerate(indices):
                     range_msg = Range()
@@ -23,8 +29,11 @@ class NeobotixDriver(Node):
                     range_msg.radiation_type = Range.ULTRASOUND
                     range_msg.min_range = 0.15
                     range_msg.range = int(data[index]) / 100.0
+                    sensor_name = sensor_names[i]
                     self.publisher.publish(range_msg)
-                    self.get_logger().info(f'Publishing: "{range_msg.range}"')
+                    self.get_logger().info(f'{sensor_name}: {range_msg.range}m')
+                self.get_logger().info('------')
+                time.sleep(0.1)   
                     
 
 def main():
